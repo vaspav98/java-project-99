7d51324e9147265fe375ea4c8b189217b09793c0
pavel <vas-pav@mail.ru>
2023-11-05 19:24:14 +0300

    start

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM gradle:8.4.0-jdk20
+
+WORKDIR /
+
+COPY / .
+
+RUN gradle installDist
+
+CMD ./build/install/app/bin/app
---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+	application
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+application {
+	mainClass.set("hexlet.code.AppApplication")
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
